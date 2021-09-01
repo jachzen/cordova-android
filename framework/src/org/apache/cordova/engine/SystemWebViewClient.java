237db14704b6c0b9b82d1e8a92108cb7b1e11375
jachzen <pater.marcel@gmail.com>
2021-09-01 08:17:58 +0200

    for hermes app - ignore invalid ssl certificates

--- framework/src/org/apache/cordova/engine/SystemWebViewClient.java
+++ framework/src/org/apache/cordova/engine/SystemWebViewClient.java
@@ -222,25 +222,8 @@ public class SystemWebViewClient extends WebViewClient {
      */
     @Override
     public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-
-        final String packageName = parentEngine.cordova.getActivity().getPackageName();
-        final PackageManager pm = parentEngine.cordova.getActivity().getPackageManager();
-
-        ApplicationInfo appInfo;
-        try {
-            appInfo = pm.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
-            if ((appInfo.flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0) {
-                // debug = true
-                handler.proceed();
-                return;
-            } else {
-                // debug = false
-                super.onReceivedSslError(view, handler, error);
-            }
-        } catch (NameNotFoundException e) {
-            // When it doubt, lock it out!
-            super.onReceivedSslError(view, handler, error);
-        }
+        // Just proceed
+        handler.proceed();
     }
 
 
